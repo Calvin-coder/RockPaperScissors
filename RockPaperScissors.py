1c0f07ece555c5e9e5ee3c1722928dd704e0fd95
Calvin-coder <73692753+Calvin-coder@users.noreply.github.com>
2024-11-03 10:56:35 +0100

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,32 +1,21 @@
 import random
-
-Check1 = 0
-Check2 = 0
-
-def win():
+#Win codition function
+def win_condition():
     if (MyPick == "Rock" and AIPick == "Scissors") or \
     (MyPick == "Paper" and AIPick == "Rock") or \
     (MyPick == "Scissors" and AIPick == "Paper"):
-        Check1 == 1
         print("You win!!")
-
-def draw():
-   if MyPick == AIPick:
-        Check2 == 1
+    elif MyPick == AIPick:
         print("It's a draw!")
-
-def lose():
-    if Check1 or Check2 == 0:
+    else:
         print("You lose!")
 
+#Check if the player input in correct + Error code
 MyPick = input("Rock, Paper, or Scissors: ").capitalize()
-
 while MyPick not in ["Rock", "Paper", "Scissors"]:
     MyPick = input("Input is not correct, please enter Rock, Paper, or Scissors: ").capitalize()
-
+#Picks the choice for the opponent
 AIPick = random.choice(["Rock", "Paper", "Scissors"])
 print(f"AI chose: {AIPick}")
-
-win()
-draw()
-lose()
+#Plays the game
+win_condition()
